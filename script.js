19ae7e53aabd8fee81d16e0eb4e21bf1a8f28c61
lucaswyd <99453165+lucaswyd@users.noreply.github.com>
2024-04-24 15:33:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,7 @@ function generateCitation() {
             var parser = new DOMParser();
             var doc = parser.parseFromString(html, 'text/html');
             var title = doc.querySelector('title').textContent;
-            var authors = Array.from(doc.querySelectorAll('meta[name="author"]')).map(author => author.getAttribute('content'));
-            var publisher = doc.querySelector('meta[property="og:site_name"]') ? doc.querySelector('meta[property="og:site_name"]').getAttribute('content') : doc.querySelector('meta[name="publisher"]').getAttribute('content');
-            var publicationDate = doc.querySelector('meta[property="article:published_time"]') ? doc.querySelector('meta[property="article:published_time"]').getAttribute('content') : doc.querySelector('meta[name="date"]').getAttribute('content');
-            var citation = '';
-            if (authors.length > 0) {
-                citation += authors.join(', ') + '. ';
-            }
-            citation += '"' + title + '." ';
-            citation += 'Website: ' + publisher + ', ';
-            citation += publicationDate + ', ';
-            citation += url + '.';
+            var citation = '"' + title + '."';
             document.getElementById('citationOutput').textContent = citation;
         })
         .catch(error => {
